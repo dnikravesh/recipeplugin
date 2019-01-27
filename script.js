a1b6f35984e57907dee04b8ee0171ed7a320f309
dnikravesh <dannynikravesh@gmail.com>
2019-01-27 14:15:01 -0800

    add jQuery

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-function printDiv(divId) {
-    var printContents = document.getElementById(divId).innerHTML;
-    w=window.open();
-    w.document.write(printContents);
-    w.print();
-    w.close();
-}
+$(document).ready(function(){
+    $("h1").click(function(){
+      $(this).hide();
+    });
+  });
