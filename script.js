11fc7f0d60d66afecb5471bb52ba75826c60fa0e
dnikravesh <dannynikravesh@gmail.com>
2019-01-29 14:28:09 -0800

    add radio button input info

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
+  $("#radio1").click(function(){
+    $("#sub1").hide();
+    $("#sub2").hide();
+
+  });
+});
+
+$(document).ready(function(){
     $("#radio2").click(function(){
-      $(".sub1").show();
+      $("#sub1").show();
+      $("#sub2").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#radio3").click(function(){
+      $("#sub1").show();
+      $("#sub2").show();
     });
   });
